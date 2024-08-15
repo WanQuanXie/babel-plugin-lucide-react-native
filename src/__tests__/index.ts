116c5781e4023c940716009b7d4f817be36dd82c
WanQuanXie <i2cherry941219@gmail.com>
2024-08-16 00:12:38 +0800

    update: used babel-plugin-tester instead of bare jest instructions

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,60 +1,10 @@
 import path from "node:path";
-import fs from "node:fs";
-import { transformFileSync } from "@babel/core";
+import { pluginTester } from "babel-plugin-tester";
 import plugin from "../index";
+import { LUCIDE_REACT_NATIVE } from "../const";
 
-describe("lucide-react-native modularized builds", () => {
-  const fixturesDir = path.join(__dirname, "fixtures");
-  const errorFixturesDir = path.join(__dirname, "fixtures", "error");
-
-  fs.readdirSync(fixturesDir)
-    .filter((caseName) => caseName !== "error")
-    .map((caseName) => {
-      const fixtureDir = path.join(fixturesDir, caseName);
-      const actualFile = path.join(fixtureDir, "actual.js");
-      const expectedFile = path.join(fixtureDir, "expected.js");
-
-      describe(`should work with ${caseName.split("-").join(" ")}`, () => {
-        // Programmatically test with the useES option both on and off
-        it("cjs", () => {
-          const actual = transformFileSync(actualFile, {
-            compact: true,
-            plugins: [plugin],
-          })?.code?.trim();
-          const expected = transformFileSync(expectedFile, {
-            compact: true,
-          })?.code?.trim();
-          expect(actual).toBe(expected);
-        });
-
-        it("esm", () => {
-          const actual = transformFileSync(actualFile, {
-            compact: true,
-            plugins: [[plugin, { useES: true }]],
-          })?.code;
-
-          // The only difference is that cjs should be replaced with esm. This way, no changes to
-          // the tests are needed to cover testing of useES.
-          const expected = transformFileSync(expectedFile, {
-            compact: true,
-          })?.code?.replace(/cjs/g, "esm");
-          expect(actual).toBe(expected);
-        });
-      });
-    });
-
-  fs.readdirSync(errorFixturesDir).map((caseName) => {
-    const fixtureDir = path.join(fixturesDir, caseName);
-    const actualFile = path.join(fixtureDir, "actual.js");
-
-    describe(`should throw an error with ${caseName.split("-").join(" ")}`, () => {
-      it(`${caseName.split("-").join(" ")}`, () => {
-        expect(function () {
-          transformFileSync(actualFile, {
-            plugins: [plugin],
-          })?.code;
-        }).toThrow();
-      });
-    });
-  });
+pluginTester({
+  plugin,
+  pluginName: LUCIDE_REACT_NATIVE,
+  fixtures: path.join(__dirname, "fixtures"),
 });
