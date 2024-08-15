04b8316b6218dff044703fbea079c9a523c9c569
WanQuanXie <i2cherry941219@gmail.com>
2024-08-15 13:31:54 +0800

    update: configured jest type specification

--- jest.js
+++ jest.js
@@ -1,10 +1,13 @@
 const { jest: jestConfig } = require("kcd-scripts/config");
 
-module.exports = Object.assign(jestConfig, {
-  // your overrides here
-
+/**
+ *  @type {import('@jest/types').Config.InitialOptions}
+ */
+const customJestConfig = {
   // for test written in Typescript, add:
   transform: {
     "\\.(ts|tsx)$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
   },
-});
+};
+
+module.exports = Object.assign(jestConfig, customJestConfig);
