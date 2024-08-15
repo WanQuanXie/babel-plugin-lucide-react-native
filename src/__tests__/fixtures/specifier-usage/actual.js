7db221f349bcf75d7a427c3dbfaeebab43ab2d26
WanQuanXie <i2cherry941219@gmail.com>
2024-08-15 11:33:20 +0800

    feat: add specifier-usage test case

---
+++ src/__tests__/fixtures/specifier-usage/actual.js
@@ -0,0 +1,1 @@
+import { Star, BookHeart, Trash2 } from "lucide-react-native";
---
+++ src/__tests__/fixtures/specifier-usage/expected.js
@@ -0,0 +1,3 @@
+import Star from "lucide-react-native/dist/cjs/icons/star";
+import BookHeart from "lucide-react-native/dist/cjs/icons/book-heart";
+import Trash2 from "lucide-react-native/dist/cjs/icons/trash-2";
