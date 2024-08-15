6170e62e1424b4f6e1d53e411289b112bba2dcba
WanQuanXie <i2cherry941219@gmail.com>
2024-08-16 01:36:14 +0800

    feat: add specifier usage with useES true test case

---
+++ src/__tests__/fixtures/specifier-usage-esm/output.js
@@ -0,0 +1,3 @@
+import Star from "lucide-react-native/dist/esm/icons/star";
+import BookHeart from "lucide-react-native/dist/esm/icons/book-heart";
+import Trash2 from "lucide-react-native/dist/esm/icons/trash-2";
